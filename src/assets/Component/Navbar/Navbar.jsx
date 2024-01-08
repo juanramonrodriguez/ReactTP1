4d9d9401d817a89c19b32be53d451fe8996ea932
juan <juanrhim@gmail.com>
2024-01-08 11:35:53 -0300

    react

--- src/assets/Component/Navbar/Navbar.jsx
+++ src/assets/Component/Navbar/Navbar.jsx
@@ -1,22 +1,26 @@
-import { NavbarContainer, NavbarInfo, NavbarLogo, NavbarUsuario } from "./NavbarStyles";
-import { FaUser, FaShoppingCart } from "react-icons/fa";
+import { MenuContainer, NavbarContainer, NavbarLinks, NavbarLogo, NavbarUsuario } from "./NavbarStyles";
+import { FaUser, FaShoppingCart,  } from "react-icons/fa";
+import { TiThMenu } from "react-icons/ti";
 function Navbar() {
   return (
 <NavbarContainer>
   <NavbarLogo>
     <img src="https://airwheel-arg.com.ar/wp-content/uploads/2020/11/logo-airwheel.png" alt="" />
   </NavbarLogo>
-  <NavbarInfo>
+  <NavbarLinks>
     <ul>
       <li>INICIO</li>
       <li>PRODUCTOS</li>
       <li>CONTACTO</li>
     </ul>
-  </NavbarInfo>
+  </NavbarLinks>
   <NavbarUsuario>
   <FaUser style={{fontSize:"25px", cursor:"pointer"}}/>
   <FaShoppingCart style={{fontSize:"25px", cursor: "pointer"}}/>
   </NavbarUsuario>
+  <MenuContainer>
+  <TiThMenu style={{ fontSize:"25px", cursor:"pointer"}}/>
+  </MenuContainer>
 </NavbarContainer>
   )
 }
--- src/assets/Component/Navbar/NavbarStyles.js
+++ src/assets/Component/Navbar/NavbarStyles.js
@@ -1,51 +1,3 @@
-/*import styled from "styled-components";
-
-
-export const NavbarContainer = styled.div`
-display: flex;
-width: 100%;
-background-color: green;
-justify-content: center;
-align-items: center;
-gap: 200px;
-position: fixed;
-
-@media (max-width:1210px){
-    gap: 50px;
-    margin: 0;
-    padding: 0;
-}
-`
-export const NavbarLogo = styled.div`
-img{
-    width: 240px;
-    height: 30px;
-}
-img:hover{
-    filter: drop-shadow(10px 8px 6px black);
-}
-`
-export const NavbarInfo = styled.div`
-display: flex;
-background-color: blue;
-ul{
-    display: flex;
-    gap: 30px;
-}
-li{
-    cursor: pointer;
-}
-li:hover{
-    border-bottom: 1px solid black;
-}
-`
-export const NavbarUsuario = styled.div`
-display: flex;
-background-color: gray;
-gap: 20px;
-
-
-`*/
 
 import styled from "styled-components";
 
@@ -76,9 +28,14 @@ export const NavbarLogo = styled.div`
        display: none;
     }
     }
+    @media (max-width:590px){
+      img{
+        display: flex;
+      }
+    }
 `;
 
-export const NavbarInfo = styled.div`
+export const NavbarLinks = styled.div`
   display: flex;
   
   ul {
@@ -97,12 +54,24 @@ export const NavbarInfo = styled.div`
       flex-direction: row;
     }
   }
-  @media (max-width:592px){
-    
+  @media (max-width:590px){
+    display: none;
   }
 `;
 
 export const NavbarUsuario = styled.div`
   display: flex;
   gap: 10px;
+  @media(max-width: 590px){
+    display: none;
+  }
 `;
+
+export const MenuContainer = styled.div`
+display: none;
+width: auto;
+height: auto;
+@media (max-width:590px){
+  display: flex;
+}
+`
