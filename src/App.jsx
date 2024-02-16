822296b4a85559e990601de999d4bf13621e0b86
juan <juanrhim@gmail.com>
2024-02-16 09:21:56 -0300

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,19 @@
-import {BrowserRouter,Routes,Route} from"react-router-dom"
+
+import Navbar from "./assets/Component/Navbar/Navbar"
 import Layout from "./assets/Component/Layout/Layout"
 import Hero from "./assets/Component/Hero/Hero"
 import SecctionCards from "./assets/Component/SecctionCards/SecctionCards"
 import Footer from "./assets/Component/Footer/Footer"
 import Categorias from "./assets/Component/Categotrias/Categorias"
 import ProductsDestacados from "./assets/Component/Productos-Destacados/ProductsDestacados"
-import Contacto from "./assets/Component/Paginas/Contacto/Contacto"
-import About from "./assets/Component/Paginas/About/About"
-import NoPage from "./assets/Component/Paginas/NoPage/NoPage"
-import Navbar from"./assets/Component/Navbar/Navbar"
 
 
 
 function App() {
   
   return (
-  <>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Layout/>}>
-        <Route index element={<Layout/>}/>
-        <Route path="contacto" element={<Contacto/>}/>
-        <Route path="nosotros" element={<About/>}/>
-        <Route path="*" element={<NoPage/>}/>
-      </Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+    
     <Navbar/>
       <Layout>
         <Hero/>
@@ -34,9 +22,11 @@ function App() {
         <Categorias/>
       </Layout>
         <Footer/>
-
-  </>
+    
+    </>
   )
 }
 
 export default App
+
+
--- src/assets/Component/Navbar/Navbar.jsx
+++ src/assets/Component/Navbar/Navbar.jsx
@@ -1,46 +1,41 @@
-import { MenuContainer, NavbarContainer, NavbarLinks, NavbarLogo, NavbarUsuario } from "./NavbarStyles";
+import {NavbarContainer, NavbarLogo, NavbarLinks, NavbarUsuario, MenuContainer}from"./StyleNavbar"
 import { FaUser, FaShoppingCart,  } from "react-icons/fa";
 import { TiThMenu } from "react-icons/ti";
-import { Outlet, Link } from "react-router-dom";
+
 
 
 function Navbar() {
   return (
     <>
-    <NavbarContainer>
-<NavbarLogo>
-  <img src="https://airwheel-arg.com.ar/wp-content/uploads/2020/11/logo-airwheel.png" alt="" />
-</NavbarLogo>
-<NavbarLinks>
-        <ul>
-          <li>
-          <Link to="/">INICIO</Link>
-          </li>
-          <li>
-           <Link to="contacto">CONTACTO</Link>
-          </li>
-          <li>
-           <Link to="about">NOSOTROS</Link>
-          </li>
-          <li>
-            PRODUCTOS
-          </li>
-        </ul>
-</NavbarLinks>
-<NavbarUsuario>
-<FaUser style={{fontSize:"25px", cursor:"pointer"}}/>
-<FaShoppingCart style={{fontSize:"25px", cursor: "pointer"}}/>
-</NavbarUsuario>
-<MenuContainer>
-<TiThMenu style={{ fontSize:"25px", cursor:"pointer"}}/>
-</MenuContainer>
+<NavbarContainer>
+  <NavbarLogo>
+    <img src="https://airwheel-arg.com.ar/wp-content/uploads/2020/11/logo-airwheel.png" alt="" />
+  </NavbarLogo>
+  <NavbarLinks>
+    <ul>
+      <li>
+        Inicio
+      </li>
+      <li>
+        Contacto
+      </li>
+      <li>
+        Nosotros
+      </li>
+    </ul>
+  </NavbarLinks>
+  <NavbarUsuario>
+  <FaUser style={{fontSize:"25px", cursor:"pointer"}}/>
+  <FaShoppingCart style={{fontSize:"25px", cursor: "pointer"}}/>
+  </NavbarUsuario>
+  <MenuContainer>
+  <TiThMenu style={{ fontSize:"25px", cursor:"pointer"}}/>
+  </MenuContainer>
 </NavbarContainer>
-<Outlet/>
-      
-</>
-  );
+
+    </>
+  )
 }
 
-
-
 export default Navbar;
+
